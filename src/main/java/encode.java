f8b8063c32cbc2e62d1060456b3f65d5f4c5a144
ma-za-kpe <makpalyy@gmail.com>
2019-04-05 14:14:40 +0200

    add encode class, getter and setter methods

---
+++ src/main/java/encode.java
@@ -0,0 +1,17 @@
+public class encode {
+    private int key;
+    private String word;
+
+    public encode(int key, String word){
+        this.key = key;
+        this.word = word;
+    }
+
+    public int getKey(){
+        return key;
+    }
+
+    public String getWord() {
+        return word;
+    }
+}
